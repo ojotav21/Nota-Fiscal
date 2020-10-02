25b3ddd264675de1d345f59e4cb2daaa9b9f5a66
João Victor Morais <60045344+ojotav21@users.noreply.github.com>
2020-10-01 21:30:23 -0300

    Update conecta.php

--- login/conecta.php
+++ login/conecta.php
@@ -17,21 +17,4 @@
 
 
 
-<?php
-    $host ="localhost";
-    $usuario ="root";
-    $senha ="";
-    $banco="loginphp";
 
-    $conexao = new MySQLi("$host","$usuario","$senha","$banco");
-
-
-    if($conexao -> connect_error){
-        echo "Erro de Conexão!!!";
-    }else {
-        // echo "Sucesso";
-    }
-
-
-
-?>
